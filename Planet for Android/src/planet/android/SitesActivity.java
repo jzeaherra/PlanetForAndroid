b2349d0eec6e4cd268feefb334d70142a6160861
Juan Antonio Zea Herranz <juanantonio.zea.herranz@alumnos.upm.es>
2012-05-19 01:04:42 +0200

    First Commit. Activities and tabview created.

---
+++ Planet for Android/src/planet/android/SitesActivity.java
@@ -0,0 +1,12 @@
+package planet.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SitesActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sites_layout);
+
+    }
+}
---
+++ Planet for Android/src/planet/android/TripsActivity.java
@@ -0,0 +1,12 @@
+package planet.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TripsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trips_layout);
+
+    }
+}
